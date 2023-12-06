da1fd5a5f14da59cc8a965229b9f28067617a513
Sean Demeritt <seandemerit13@gmail.com>
2023-12-05 19:09:01 -0500

    profile css tweaks

--- src/routes/(app)/profile/[uid]/+layout.svelte
+++ src/routes/(app)/profile/[uid]/+layout.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { Bars3, Icon } from 'svelte-hero-icons';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 	export let data;
-
+	onMount(() => {
+		console.log($page);
+	});
 	$: userProfile = data.userProfile;
 	$: isUserProfile = data.isUserProfile;
+	$: isActive = (type: string) => $page.url.pathname === createPath(type);
+	$: createPath = (type: string) => `/profile/${userProfile.uid}/${type}`;
 </script>
 
 <div class="flex justify-center mt-4">
-	<div class="flex flex-col items-center p-4 bg-white rounded-lg shadow-s card w-130 pb-1 pt-0">
+	<div class="flex flex-col items-center bg-white rounded-lg shadow-s card w-130 pb-1">
 		<div class="flex flex-row w-full">
 			<div class="flex flex-row flex-grow items-center">
 				<div class="flex-shrink-0 avatar">
-					<div class="w-32 rounded-xl ronded-full">
+					<div class="w-28 rounded-xl ronded-full">
 						<img
 							src={userProfile.profileUrl ||
 								'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxroUOuhHpV9KBpOuiYJSvok9YOgMoxGfFnw&s'}
@@ -50,38 +55,39 @@
 				{/if}
 			</div>
 		</div>
-		<div class="flex text-gray-500 pb-3">
+		<div class="flex text-gray-500 mb-3">
 			{userProfile.bio}
 		</div>
-		<div class="flex flex-row w-full">
-			<div class="navbar bg-base-100">
-				<div class="navbar-start">
-					<div class="dropdown">
-						<label tabindex="0" class="dropdown btn lg:hidden" for="bars-icon">
-							<Icon src={Bars3} class="h-6 w-10" id="bars-icon" />
-						</label>
-						<ul
-							class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-							role="menu"
-							aria-label="Profile Menu"
-						>
-							<li><a href="/profile/{userProfile.uid}/sweets" role="menuitem">Sweets</a></li>
-							<li><a href="/profile/{userProfile.uid}/comments" role="menuitem">Comments</a></li>
-							<li><a href="/profile/{userProfile.uid}/resweets" role="menuitem">Resweets</a></li>
-							<li><a href="/profile/{userProfile.uid}/followers" role="menuitem">Followers</a></li>
-							<li><a href="/profile/{userProfile.uid}/following" role="menuitem">Following</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="navbar-center flex justify-around font-bold hidden border-0 pb-0 lg:flex">
-					<a href="/profile/{userProfile.uid}/likes">Likes</a>
-					<a href="/profile/{userProfile.uid}/sweets">Sweets</a>
-					<a href="/profile/{userProfile.uid}/comments">Comments</a>
-					<a href="/profile/{userProfile.uid}/resweets">Resweets</a>
-					<a href="/profile/{userProfile.uid}/followers">Followers</a>
-					<a href="/profile/{userProfile.uid}/following">Following</a>
-				</div>
-			</div>
+		<div class="flex flex-row w-full justify-around pb-4 mb-3 border-b-1">
+			<a
+				class="text-lg {isActive('followers') ? 'border-b-1 border-black' : ''}"
+				href={createPath('followers')}
+				><span class="font-bold mr-2">100</span> <span class="text-gray-500">Followers</span></a
+			>
+			<a
+				class="text-lg {isActive('following') ? 'border-b-1 border-black' : ''}"
+				href={createPath('following')}
+				><span class="font-bold mr-2">100</span> <span class="text-gray-500">Following</span></a
+			>
+		</div>
+
+		<div class="flex flex-row w-full text-lg navbar-center font-bold justify-around pb-0 mb-1">
+			<a
+				class="flex {isActive('likes') ? 'border-b-1 border-black' : ''}"
+				href="/profile/{userProfile.uid}/likes">Likes</a
+			>
+			<a
+				class="flex {isActive('sweets') ? 'border-b-1 border-black' : ''}"
+				href={createPath('sweets')}>Sweets</a
+			>
+			<a
+				class="flex {isActive('comments') ? 'border-b-1 border-black' : ''}"
+				href={createPath('comments')}>Comments</a
+			>
+			<a
+				class="flex {isActive('resweets') ? 'border-b-1 border-black' : ''}"
+				href={createPath('resweets')}>Resweets</a
+			>
 		</div>
 	</div>
 </div>
