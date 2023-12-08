3b1f9f6d34e8f5b240c21a08e427d102396336d3
Sean Demeritt <seandemerit13@gmail.com>
2023-12-08 13:45:37 -0500

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,24 @@
 <script>
-	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-	injectSpeedInsights();
 	import './styles.css';
+	import { browser } from '$app/environment';
+	import { webVitals } from '$lib/utils/vitals.js';
+	import { page } from '$app/stores';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	export let data;
+
+	inject({ mode: dev ? 'development' : 'production' });
+
+	injectSpeedInsights();
+
+	$: if (browser && data?.analyticsId) {
+		webVitals({
+			path: $page.url.pathname,
+			params: $page.params,
+			analyticsId: data.analyticsId
+		});
+	}
 </script>
 
 <slot />
