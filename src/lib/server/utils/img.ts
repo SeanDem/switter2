8b867a15bb67e63b7806fdb02dd90163c79adf00
Sean Demeritt <seandemerit13@gmail.com>
2023-12-13 10:33:37 -0500

    fix

--- src/lib/server/utils/img.ts
+++ src/lib/server/utils/img.ts
@@ -1,9 +1,27 @@
 import Jimp from 'jimp';
 
 export async function resizeAndCompressImage(file: File, fileName: String): Promise<File> {
-	const buffer = Buffer.from(await file.arrayBuffer());
-	const image = await Jimp.read(buffer);
-	image.resize(500, Jimp.AUTO).quality(75);
-	const outputBuffer = await image.getBufferAsync(Jimp.MIME_JPEG);
-	return new File([outputBuffer], `${fileName}.jpeg`, { type: 'image/jpeg' });
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const image = await Jimp.read(buffer);
+
+    image.background(0xffffffff);
+
+    const aspectRatio = image.bitmap.width / image.bitmap.height;
+
+    let newWidth, newHeight;
+
+    if (aspectRatio >= 1) {
+        newWidth = 300;
+        newHeight = 300 / aspectRatio;
+    } else {
+        newHeight = 300;
+        newWidth = 300 * aspectRatio;
+    }
+
+    image.resize(newWidth, newHeight);
+    image.quality(65);
+
+    const outputBuffer = await image.getBufferAsync(Jimp.MIME_JPEG);
+
+    return new File([outputBuffer], `${fileName}.jpeg`, { type: 'image/jpeg' });
 }
